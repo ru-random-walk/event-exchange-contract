d23d617472477e80ad2114a2740e32e542ee0e3b
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-12-02 22:52:58 +0300

    add java doc

--- src/main/java/ru/random/walk/dto/CreatePrivateChatEvent.java
+++ src/main/java/ru/random/walk/dto/CreatePrivateChatEvent.java
@@ -6,6 +6,9 @@ import java.util.UUID;
  * Data Transfer Object (DTO) representing an event to create a private chat with two members.
  * <p>
  * This event must be used with the {@link ru.random.walk.kafka.EventTopic#CREATE_CHAT} Kafka topic.
+ *
+ * @param chatMember1 The UUID of the first chat member.
+ * @param chatMember2 The UUID of the second chat member.
  */
 public record CreatePrivateChatEvent(
         UUID chatMember1,
--- src/main/java/ru/random/walk/kafka/EventTopic.java
+++ src/main/java/ru/random/walk/kafka/EventTopic.java
@@ -1,8 +1,18 @@
 package ru.random.walk.kafka;
 
 /**
+ * Class representing Kafka event topics.
  * Topic name must be matches with Pattern: &lt;event&gt;.&lt;entity&gt;
  */
 public class EventTopic {
+    /**
+     * The Kafka topic for creating a private chat.
+     */
     public static final String CREATE_CHAT = "create.chat";
+
+    /**
+     * Default private constructor
+     */
+    private EventTopic(){
+    }
 }
